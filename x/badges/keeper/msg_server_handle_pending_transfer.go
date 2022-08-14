bdcbae91f7c9a48d8293d4cac25b40059fb39ffa
trevormil <trevormil@comcast.net>
2022-08-13 21:07:09 -0400

    switch to only iterate pending once

--- x/badges/keeper/msg_server_handle_pending_transfer.go
+++ x/badges/keeper/msg_server_handle_pending_transfer.go
@@ -45,115 +45,117 @@ func (k msgServer) HandlePendingTransfer(goCtx context.Context, msg *types.MsgHa
 	balanceInfoCache := make(map[uint64]types.UserBalanceInfo, 0)
 	didUpdateBalanceInfo := make(map[uint64]bool, 0)
 
-	//TODO: In the future, we can make this a binary search since this is all sorted by the nonces (append-only)
-	for _, nonceRange := range msg.NonceRanges {
-		if nonceRange.End == 0 {
-			nonceRange.End = nonceRange.Start
-		}
+	for idx, CurrPendingTransfer := range creatorBalanceInfo.Pending {
+		for _, nonceRange := range msg.NonceRanges {
+			if nonceRange.End == 0 {
+				nonceRange.End = nonceRange.Start
+			}
 
-		for idx, CurrPendingTransfer := range creatorBalanceInfo.Pending {
 			if CurrPendingTransfer.ThisPendingNonce <= nonceRange.End && CurrPendingTransfer.ThisPendingNonce >= nonceRange.Start {
-
-				// Handle accepting after expiration time
-				expired := uint64(ctx.BlockTime().Unix()) > CurrPendingTransfer.ExpirationTime
-				if msg.Accept && CurrPendingTransfer.ExpirationTime != 0 {
-					if expired {
-						return nil, ErrPendingTransferExpired
-					}
-				} 
 				
-				pastCancelDeadline := true //if set to 0, it is cancellable at any time
-				if CurrPendingTransfer.CantCancelBeforeTime != 0 {
-					pastCancelDeadline = uint64(ctx.BlockTime().Unix()) > CurrPendingTransfer.CantCancelBeforeTime
+			} else {
+				continue
+			}
+
+			// Handle accepting after expiration time
+			expired := uint64(ctx.BlockTime().Unix()) > CurrPendingTransfer.ExpirationTime
+			if msg.Accept && CurrPendingTransfer.ExpirationTime != 0 {
+				if expired {
+					return nil, ErrPendingTransferExpired
 				}
+			} 
+			
+			pastCancelDeadline := true //if set to 0, it is cancellable at any time
+			if CurrPendingTransfer.CantCancelBeforeTime != 0 {
+				pastCancelDeadline = uint64(ctx.BlockTime().Unix()) > CurrPendingTransfer.CantCancelBeforeTime
+			}
 
-				
+			
 
-				outgoingTransfer := CurrPendingTransfer.From == CreatorAccountNum
+			outgoingTransfer := CurrPendingTransfer.From == CreatorAccountNum
 
-				//Normal transfer flags
-				acceptOwnOutgoingTransfer := CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
-				acceptIncomingTransfer := !CurrPendingTransfer.Sent && msg.Accept && !outgoingTransfer
-				cancelOwnOutgoingTransfer := CurrPendingTransfer.Sent && !msg.Accept && outgoingTransfer
-				rejectIncomingTransfer := !CurrPendingTransfer.Sent && !msg.Accept
+			//Normal transfer flags
+			acceptOwnOutgoingTransfer := CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
+			acceptIncomingTransfer := !CurrPendingTransfer.Sent && msg.Accept && !outgoingTransfer
+			cancelOwnOutgoingTransfer := CurrPendingTransfer.Sent && !msg.Accept && outgoingTransfer
+			rejectIncomingTransfer := !CurrPendingTransfer.Sent && !msg.Accept
 
-				if acceptOwnOutgoingTransfer {
-					return nil, ErrCantAcceptOwnTransferRequest
+			if acceptOwnOutgoingTransfer {
+				return nil, ErrCantAcceptOwnTransferRequest
+			}
+
+			//Pending transfer flags
+			finalizeOwnTransferRequestAfterApprovedByOtherParty := CurrPendingTransfer.Sent && msg.Accept && !outgoingTransfer
+			acceptTransferRequestButMarkAsApproved := !msg.ForcefulAccept && !CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
+			acceptTransferRequestForcefully := msg.ForcefulAccept && !CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
+			rejectTransferRequest := !CurrPendingTransfer.Sent && !msg.Accept
+			cancelOwnTransferRequest := CurrPendingTransfer.Sent && !msg.Accept && !outgoingTransfer
+
+			if !pastCancelDeadline && (cancelOwnOutgoingTransfer || cancelOwnTransferRequest) {
+				return nil, ErrCantCancelYet
+			}
+
+			//Other helper flags
+			onlyUpdatingCreatorBalance := rejectIncomingTransfer || rejectTransferRequest || acceptTransferRequestButMarkAsApproved
+			otherPartyMayHaveRemovedAlready := cancelOwnOutgoingTransfer || cancelOwnTransferRequest
+			needToRemoveAtLeastOneFromPending := !acceptTransferRequestButMarkAsApproved
+
+			//Get basic information about the other party and update cache if not already in cache
+			otherPartyAccountNum := CurrPendingTransfer.From
+			if outgoingTransfer {
+				otherPartyAccountNum = CurrPendingTransfer.To
+			}
+			otherPartyBalanceKey := ConstructBalanceKey(otherPartyAccountNum, msg.BadgeId)
+			otherPartyNonce := CurrPendingTransfer.OtherPendingNonce
+			otherPartyBalanceInfo, ok := balanceInfoCache[otherPartyAccountNum]
+			if !ok && !onlyUpdatingCreatorBalance {
+				otherPartyBalanceInfo, found = k.GetUserBalanceFromStore(ctx, otherPartyBalanceKey)
+				balanceInfoCache[otherPartyAccountNum] = otherPartyBalanceInfo
+				if !found {
+					return nil, ErrUserBalanceNotExists
 				}
-
-				//Pending transfer flags
-				finalizeOwnTransferRequestAfterApprovedByOtherParty := CurrPendingTransfer.Sent && msg.Accept && !outgoingTransfer
-				acceptTransferRequestButMarkAsApproved := !msg.ForcefulAccept && !CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
-				acceptTransferRequestForcefully := msg.ForcefulAccept && !CurrPendingTransfer.Sent && msg.Accept && outgoingTransfer
-				rejectTransferRequest := !CurrPendingTransfer.Sent && !msg.Accept
-				cancelOwnTransferRequest := CurrPendingTransfer.Sent && !msg.Accept && !outgoingTransfer
-
-				if !pastCancelDeadline && (cancelOwnOutgoingTransfer || cancelOwnTransferRequest) {
-					return nil, ErrCantCancelYet
-				}
-
-				//Other helper flags
-				onlyUpdatingCreatorBalance := rejectIncomingTransfer || rejectTransferRequest || acceptTransferRequestButMarkAsApproved
-				otherPartyMayHaveRemovedAlready := cancelOwnOutgoingTransfer || cancelOwnTransferRequest
-				needToRemoveAtLeastOneFromPending := !acceptTransferRequestButMarkAsApproved
-
-				//Get basic information about the other party and update cache if not already in cache
-				otherPartyAccountNum := CurrPendingTransfer.From
-				if outgoingTransfer {
-					otherPartyAccountNum = CurrPendingTransfer.To
-				}
-				otherPartyBalanceKey := ConstructBalanceKey(otherPartyAccountNum, msg.BadgeId)
-				otherPartyNonce := CurrPendingTransfer.OtherPendingNonce
-				otherPartyBalanceInfo, ok := balanceInfoCache[otherPartyAccountNum]
-				if !ok && !onlyUpdatingCreatorBalance {
-					otherPartyBalanceInfo, found = k.GetUserBalanceFromStore(ctx, otherPartyBalanceKey)
-					balanceInfoCache[otherPartyAccountNum] = otherPartyBalanceInfo
-					if !found {
-						return nil, ErrUserBalanceNotExists
-					}
-				}
-				
-				if acceptIncomingTransfer {
-					creatorBalanceInfo, err = AddBalancesForIdRanges(ctx, creatorBalanceInfo, []*types.IdRange{CurrPendingTransfer.SubbadgeRange}, CurrPendingTransfer.Amount)
-				} else if acceptTransferRequestButMarkAsApproved {
-					creatorBalanceInfo.Pending[idx].MarkedAsAccepted = true
-				} else if cancelOwnOutgoingTransfer {
-					creatorBalanceInfo, err = RevertEscrowedBalancesAndApprovals(ctx, creatorBalanceInfo, *CurrPendingTransfer.SubbadgeRange, CurrPendingTransfer.From, CurrPendingTransfer.ApprovedBy, CurrPendingTransfer.Amount)
-				} else if finalizeOwnTransferRequestAfterApprovedByOtherParty {
-					idx, found := SearchPendingByNonce(otherPartyBalanceInfo.Pending, otherPartyNonce)
-					if found {
-						if !otherPartyBalanceInfo.Pending[idx].MarkedAsAccepted {
-							return nil, ErrNotApproved
-						}
-					} else {
+			}
+			
+			if acceptIncomingTransfer {
+				creatorBalanceInfo, err = AddBalancesForIdRanges(ctx, creatorBalanceInfo, []*types.IdRange{CurrPendingTransfer.SubbadgeRange}, CurrPendingTransfer.Amount)
+			} else if acceptTransferRequestButMarkAsApproved {
+				creatorBalanceInfo.Pending[idx].MarkedAsAccepted = true
+			} else if cancelOwnOutgoingTransfer {
+				creatorBalanceInfo, err = RevertEscrowedBalancesAndApprovals(ctx, creatorBalanceInfo, *CurrPendingTransfer.SubbadgeRange, CurrPendingTransfer.From, CurrPendingTransfer.ApprovedBy, CurrPendingTransfer.Amount)
+			} else if finalizeOwnTransferRequestAfterApprovedByOtherParty {
+				idx, found := SearchPendingByNonce(otherPartyBalanceInfo.Pending, otherPartyNonce)
+				if found {
+					if !otherPartyBalanceInfo.Pending[idx].MarkedAsAccepted {
 						return nil, ErrNotApproved
 					}
-
-					otherPartyBalanceInfo, creatorBalanceInfo, err = ForcefulTransfer(ctx, badge, *CurrPendingTransfer.SubbadgeRange, otherPartyBalanceInfo, creatorBalanceInfo, CurrPendingTransfer.Amount, CurrPendingTransfer.From, CurrPendingTransfer.To, CurrPendingTransfer.From, CurrPendingTransfer.ExpirationTime)
-				} else if acceptTransferRequestForcefully {
-					creatorBalanceInfo, otherPartyBalanceInfo, err = ForcefulTransfer(ctx, badge, *CurrPendingTransfer.SubbadgeRange, creatorBalanceInfo, otherPartyBalanceInfo, CurrPendingTransfer.Amount, CurrPendingTransfer.From, CurrPendingTransfer.To, CreatorAccountNum, CurrPendingTransfer.ExpirationTime)
+				} else {
+					return nil, ErrNotApproved
 				}
 
+				otherPartyBalanceInfo, creatorBalanceInfo, err = ForcefulTransfer(ctx, badge, *CurrPendingTransfer.SubbadgeRange, otherPartyBalanceInfo, creatorBalanceInfo, CurrPendingTransfer.Amount, CurrPendingTransfer.From, CurrPendingTransfer.To, CurrPendingTransfer.From, CurrPendingTransfer.ExpirationTime)
+			} else if acceptTransferRequestForcefully {
+				creatorBalanceInfo, otherPartyBalanceInfo, err = ForcefulTransfer(ctx, badge, *CurrPendingTransfer.SubbadgeRange, creatorBalanceInfo, otherPartyBalanceInfo, CurrPendingTransfer.Amount, CurrPendingTransfer.From, CurrPendingTransfer.To, CreatorAccountNum, CurrPendingTransfer.ExpirationTime)
+			}
+
+			if err != nil {
+				return nil, err
+			}
+			
+
+			if needToRemoveAtLeastOneFromPending {
+				creatorBalanceInfo, err = RemovePending(ctx, creatorBalanceInfo, CurrPendingTransfer.ThisPendingNonce, otherPartyNonce)
 				if err != nil {
 					return nil, err
 				}
-				
 
-				if needToRemoveAtLeastOneFromPending {
-					creatorBalanceInfo, err = RemovePending(ctx, creatorBalanceInfo, CurrPendingTransfer.ThisPendingNonce, otherPartyNonce)
+				//Try to remove from the other party's pending, but in some situations it may have already been removed
+				if !onlyUpdatingCreatorBalance {
+					otherPartyBalanceInfo, err = RemovePending(ctx, otherPartyBalanceInfo, otherPartyNonce, CurrPendingTransfer.ThisPendingNonce)
+					didUpdateBalanceInfo[otherPartyAccountNum] = true
+					balanceInfoCache[otherPartyAccountNum] = otherPartyBalanceInfo
 					if err != nil {
-						return nil, err
-					}
-
-					//Try to remove from the other party's pending, but in some situations it may have already been removed
-					if !onlyUpdatingCreatorBalance {
-						otherPartyBalanceInfo, err = RemovePending(ctx, otherPartyBalanceInfo, otherPartyNonce, CurrPendingTransfer.ThisPendingNonce)
-						didUpdateBalanceInfo[otherPartyAccountNum] = true
-						balanceInfoCache[otherPartyAccountNum] = otherPartyBalanceInfo
-						if err != nil {
-							if !(err == ErrPendingNotFound && otherPartyMayHaveRemovedAlready) {
-								return nil, err
-							}
+						if !(err == ErrPendingNotFound && otherPartyMayHaveRemovedAlready) {
+							return nil, err
 						}
 					}
 				}
