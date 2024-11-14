a236396f8bd67b8d2b2c7c5e2717e946870241c0
Trevor Miller <trevormil@comcast.net>
2024-11-14 13:01:21 -0500

    make migrate only blockstore / tx_index

--- scripts/migrate.go
+++ scripts/migrate.go
@@ -13,6 +13,11 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
+//NOTE: This only handles blockstore and tx_index.db
+//      It does not handle application.db, state.db, or evidence.db or cs.wal
+//I tried and state / evidence seemed to be enough for the node to start up, but
+//ran into trouble when I tried to add application.db back in
+
 func main() {
 	// Define command line flags
 	sourcePtr := flag.String("source", "", "Path to the source snapshot directory (required)")
@@ -32,10 +37,25 @@ func main() {
 	sourceDBPath := *sourcePtr
 	targetDBPath := *targetPtr
 
+	// applicationSnapshotDB, err := leveldb.OpenFile(sourceDBPath+"/application.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	txIndexSnapshotDB, err := leveldb.OpenFile(sourceDBPath+"/tx_index.db", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// stateSnapshotDB, err := leveldb.OpenFile(sourceDBPath+"/state.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// evidenceSnapshotDB, err := leveldb.OpenFile(sourceDBPath+"/evidence.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	blockstoreSnapshotDB, err := leveldb.OpenFile(sourceDBPath+"/blockstore.db", nil)
 	if err != nil {
@@ -52,10 +72,70 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// targetStateDB, err := leveldb.OpenFile(targetDBPath+"/state.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// targetEvidenceDB, err := leveldb.OpenFile(targetDBPath+"/evidence.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// targetApplicationDB, err := leveldb.OpenFile(targetDBPath+"/application.db", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	defer txIndexSnapshotDB.Close()
 	defer blockstoreSnapshotDB.Close()
 	defer targetTxIndexDB.Close()
 	defer targetBlockstoreDB.Close()
+	// defer targetStateDB.Close()
+	// defer targetEvidenceDB.Close()
+	// defer targetApplicationDB.Close()
+	// defer applicationSnapshotDB.Close()
+	// defer stateSnapshotDB.Close()
+	// defer evidenceSnapshotDB.Close()
+
+	// iter := applicationSnapshotDB.NewIterator(nil, nil)
+	// i := 0
+	// for iter.Next() {
+	// 	key := iter.Key()
+	// 	value := iter.Value()
+	// 	i++
+	// 	// if i%100 == 0 {
+	// 	// 	fmt.Printf("Key: %X\n", key)
+	// 	// }
+	// 	if _, err := targetApplicationDB.Get(key, nil); err == nil {
+	// 		continue
+	// 	}
+
+	// 	targetApplicationDB.Put(key, value, nil)
+	// }
+
+	// iter = stateSnapshotDB.NewIterator(nil, nil)
+	// for iter.Next() {
+	// 	key := iter.Key()
+	// 	value := iter.Value()
+	// 	if _, err := targetStateDB.Get(key, nil); err == nil {
+	// 		fmt.Printf("Skipping key: %X\n", key)
+	// 		continue
+	// 	}
+
+	// 	targetStateDB.Put(key, value, nil)
+	// }
+
+	// iter = evidenceSnapshotDB.NewIterator(nil, nil)
+	// for iter.Next() {
+	// 	key := iter.Key()
+	// 	value := iter.Value()
+	// 	if _, err := targetEvidenceDB.Get(key, nil); err == nil {
+	// 		continue
+	// 	}
+
+	// 	targetEvidenceDB.Put(key, value, nil)
+	// }
 
 	//Iterate over the tx_index.db and copy the keys and values to the targetDB
 	//If we already have the key in the targetDB, skip it and do not overwrite
