f6f9a7e1e7df2ab0012f41993837df8d451ab6a7
Trevor Miller <trevormil@comcast.net>
2025-04-29 06:52:03 -0400

    add upgrade handlers

---
+++ app/upgrades/v1/upgrades.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/bitbadges/bitbadgeschain/x/badges/keeper"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const (
+	UpgradeName = "v1"
+)
+
+// CreateUpgradeHandler creates an SDK upgrade handler for v1
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	badgesKeeper keeper.Keeper,
+) func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// Run migrations
+		if err := badgesKeeper.MigrateBadgesKeeper(sdk.UnwrapSDKContext(ctx)); err != nil {
+			return nil, err
+		}
+
+		// Run module migrations
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
