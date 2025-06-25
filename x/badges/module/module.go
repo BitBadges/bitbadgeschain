03fc3a1e6003a886b71ab726c212f549f3887c29
Trevor Miller <trevormil@comcast.net>
2025-06-25 08:38:25 -0400

    .

--- x/badges/module/module.go
+++ x/badges/module/module.go
@@ -127,7 +127,7 @@ func NewAppModule(
 	}
 }
 
-const ConsensusVersion = 7
+const ConsensusVersion = 8
 
 // RegisterServices registers a gRPC query service to respond to the module-specific gRPC queries
 func (am AppModule) RegisterServices(cfg module.Configurator) {
