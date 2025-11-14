cd34eac64a1d6a9b43487acd4e5d4726c6102a32
Trevor Miller <trevormil@comcast.net>
2025-11-14 12:27:18 -0500

    add authoriy as well allowed to do anything

--- x/badges/keeper/validation.go
+++ x/badges/keeper/validation.go
@@ -15,8 +15,18 @@ type UniversalValidationParams struct {
 
 // Validates everything about the Msg is valid and returns (creatorNum, collection, permissions, error).
 func (k Keeper) UniversalValidate(ctx sdk.Context, collection *types.TokenCollection, params UniversalValidationParams) error {
+
 	// Assert all permissions
 	if params.MustBeManager {
+		// Check if gov authority
+		govAuthorized := k.GetAuthority() == params.Creator
+		if govAuthorized {
+			// Gov authority is allowed to do anything
+			// Technically, the gov authority is allowed to do anything already through manual upgrades, etc
+			// This just streamlines it to be able to use execute Msgs directly from the proposal
+			return nil
+		}
+
 		currManager := types.GetCurrentManager(ctx, collection)
 		if currManager != params.Creator {
 			return sdkerrors.Wrapf(ErrSenderIsNotManager, "current manager is %s but got %s", currManager, params.Creator)
